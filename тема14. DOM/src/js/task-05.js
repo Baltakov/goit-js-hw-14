f309029207065c99e889ea1b2092f5f5dffffc38
Baltakov <tira2500@gmail.com>
2023-10-18 05:04:54 +0300

    update files

--- тема14. DOM/src/js/task-05.js
+++
@@ -1,6 +0,0 @@
-const nameInput = document.getElementById("name-input");
-const nameOutput = document.getElementById("name-output");
-
-nameInput.addEventListener("input", (event) => {
-  nameOutput.textContent = event.target.value || "Anonymous";
-});
